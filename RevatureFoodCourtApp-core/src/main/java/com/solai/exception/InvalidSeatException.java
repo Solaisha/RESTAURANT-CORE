fb0ef585f53578000da194609f3d7b8f4395cad2
DELL <DELL@DELL-PC>
2017-02-08 20:07:11 +0530

    Exception

---
+++ RevatureFoodCourtApp-core/src/main/java/com/solai/exception/InvalidSeatException.java
@@ -0,0 +1,21 @@
+package com.solai.exception;
+
+public class InvalidSeatException extends Exception
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidSeatException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidSeatException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
